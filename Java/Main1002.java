5e98ba9ed11f4f370554ff089ce954c6ecc729c6
Gabriel Leoncio Verdi <101580365+glVerdi@users.noreply.github.com>
2025-03-11 14:05:25 +0000

    Modificando 1002 em java

--- Java/Main1002.java
+++ Java/Main1002.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main1002 {
     public static void main (String args[]) throws IOException {
@@ -7,7 +8,7 @@ public class Main1002 {
         double pi = 3.14159;
         double raio, area;
         raio = sc.nextDouble();
-        area = pi * (raio * raio);
+        area = pi * pow(raio, 2.0);
         System.out.printf("A=%.4f\n", area);
         sc.close();
     }
