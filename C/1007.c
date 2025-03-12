650ad3dd894dc4a8418b235904a3711ecbbfc4f1
Gabriel Leoncio Verdi <101580365+glVerdi@users.noreply.github.com>
2025-03-12 19:19:18 +0000

    Exercicio 1007 em Java, Python e C

---
+++ C/1007.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void) {
+    int A, B, C, D, Diferenca;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+    Diferenca = (A * B) - (C * D);
+    printf("DIFERENCA = %d\n", Diferenca);
+    return 0;
+}
---
+++ Java/Main1007.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main1007 {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int A, B, C, D, diferenca;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+        diferenca = (A * B) - (C * D);
+        System.out.println("DIFERENCA = " + diferenca);
+        sc.close();
+    }
+ 
+}
